a6dc38d77ff94cc8561f20f5140284646916f128
tombrov <tbrovy@gmail.com>
2021-01-15 09:52:33 +0200

    dev

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from redis import Redis
 import os
 
 
@@ -10,7 +11,11 @@ def test_score_service():
     chrome_driver = os.path.join("./chromedriver")
     driver = webdriver.Chrome(chrome_options=options, executable_path='./chromedriver')
 
-    driver.get("http://127.0.0.1:5000")
+    r = Redis()
+    r.set('user', 8)
+    r.close()
+
+    driver.get("http://0.0.0.0:8777")
     score = int(driver.find_element_by_id("score").get_attribute("innerText"))
     if 1 < score < 1000:
         x = True
